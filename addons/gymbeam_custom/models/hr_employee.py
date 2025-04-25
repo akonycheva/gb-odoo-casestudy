a785a46f749a07c98956bdd17763e29923ee990f
Anfisa <anfa.conich@gmail.com>
2025-04-25 21:56:44 +0200

    add fields salary, tax, total salary, special_phone

--- addons/gymbeam_custom/models/hr_employee.py
+++ addons/gymbeam_custom/models/hr_employee.py
@@ -1,6 +1,21 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
     i_love_gb = fields.Boolean(string='I Love GymBeam', default=True)
+
+    salary = fields.Integer(string='Salary')
+    tax = fields.Integer(string='Tax')
+    total_salary = fields.Integer(string='Total Salary', compute='_compute_total_salary',store=False)
+
+    special_phone = fields.Char(string='Special Phone')
+
+    @api.depends('salary', 'tax')
+    def _compute_total_salary(self):
+         self.total_salary = self.salary + self.tax
+
+    def write(self, vals):
+        if 'special_phone' in vals and not vals['special_phone']:
+            vals['special_phone'] = '0901123456'
+        return super(HrEmployee, self).write(vals)
