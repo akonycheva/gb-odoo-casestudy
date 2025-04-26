df4285bfa9cbc4c5e2d6ee6ccf3993bf818d8399
Anfisa <anfa.conich@gmail.com>
2025-04-26 10:04:48 +0200

    add unique employee number

--- addons/gymbeam_custom/models/hr_applicant.py
+++ addons/gymbeam_custom/models/hr_applicant.py
@@ -1,4 +1,19 @@
-from odoo import models
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class HrApplicant(models.Model):
     _inherit = 'hr.applicant'
+    employee_number = fields.Char(string='Employee Number', required=True, tracking=True)
+
+    @api.constrains('employee_number')
+    def _check_unique_employee_number(self):
+        for record in self:
+            # Проверяем, есть ли такой номер в hr.applicant (кроме текущей записи)
+            domain_applicant = [('employee_number', '=', record.employee_number), ('id', '!=', record.id)]
+            if self.env['hr.applicant'].search(domain_applicant):
+                raise ValidationError("Employee Number must be unique across Applicants!")
+
+            # Проверяем, есть ли такой номер в hr.employee
+            domain_employee = [('employee_number', '=', record.employee_number)]
+            if self.env['hr.employee'].search(domain_employee):
+                raise ValidationError("Employee Number must be unique across Employees!")
--- addons/gymbeam_custom/models/hr_employee.py
+++ addons/gymbeam_custom/models/hr_employee.py
@@ -1,4 +1,5 @@
 from odoo import fields, models, api
+from odoo.exceptions import ValidationError
 
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
@@ -13,6 +14,8 @@ class HrEmployee(models.Model):
 
     employee_contacts = fields.Binary(string='Employee Contacts')
 
+    employee_number = fields.Char(string='Employee Number', required=True, tracking=True)
+
     @api.depends('salary', 'tax')
     def _compute_total_salary(self):
          self.total_salary = self.salary + self.tax
@@ -21,3 +24,14 @@ class HrEmployee(models.Model):
         if 'special_phone' in vals and not vals['special_phone']:
             vals['special_phone'] = '0901123456'
         return super(HrEmployee, self).write(vals)
+
+    @api.constrains('employee_number')
+    def _check_unique_employee_number(self):
+        for record in self:
+            domain_employee = [('employee_number', '=', record.employee_number), ('id', '!=', record.id)]
+            if self.env['hr.employee'].search(domain_employee):
+                raise ValidationError("Employee Number must be unique across Employees!")
+
+            domain_applicant = [('employee_number', '=', record.employee_number)]
+            if self.env['hr.applicant'].search(domain_applicant):
+                raise ValidationError("Employee Number must be unique across Applicants!")
